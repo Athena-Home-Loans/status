be61584c2ac1527430054cf84a628f2c4a01e314
Garth <garth@athena.com.au>
2024-12-17 11:56:45 +1100

    Updates.

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer style={{ marginTop: "auto", paddingTop: 32 }}>
     <p
-      style={{ fontSize: 14, lineHeight: 20 / 16, fontWeight: 400, margin: 0 }}
+      style={{ fontSize: 12, lineHeight: 14 / 12, fontWeight: 400, margin: 0 }}
     >
       ATHENA MORTGAGE PTY LTD | ABN 24 619 536 506 | AUSTRALIAN CREDIT LICENCE
       502611
